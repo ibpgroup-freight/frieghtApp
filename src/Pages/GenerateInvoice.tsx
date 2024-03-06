d60346a47c7bbab0f52ea0631e402c9c9e61c1fe
Nouman <noumankhan95@yahoo.com>
2024-03-07 04:47:08 +0500

    fixes

--- src/Pages/GenerateInvoice.tsx
+++ src/Pages/GenerateInvoice.tsx
@@ -767,6 +767,7 @@ function GenerateInvoice() {
     resetInquiry,
     setaddress,
     setPrestationArray,
+    prestation,
   } = useInquiryItem();
 
   const {
@@ -2106,6 +2107,90 @@ function GenerateInvoice() {
                   </div>
                 </div>
               </div>
+              {formikObj.values.type === "Quotation" && (
+                <div className="w-full space-y-2 w-5/5">
+                  <h1 className="text-xl text-center text-blue-900 font-serif">
+                    Prestations
+                  </h1>
+                  <div className="mx-auto w-[90%] overflow-auto mt-20">
+                    <table className="border overflow-x-auto w-full ml-30 border-slate-400 md:border-spacing-x-10 md:border-spacing-y-2">
+                      <thead>
+                        <tr>
+                          <th className="border border-slate-300 p-4 bg-blue-50 w-auto">
+                            index
+                          </th>
+                          <th className="border border-slate-300 p-4 bg-blue-50 w-auto">
+                            Description
+                          </th>
+                          <th className="border border-slate-300 p-4 bg-blue-50 w-auto">
+                            Currency
+                          </th>
+                          <th className="border border-slate-300 p-4 bg-blue-50 w-auto">
+                            Total
+                          </th>
+                        </tr>
+                      </thead>
+                      {
+                        <tbody>
+                          {prestation?.map((i, index) => (
+                            <tr key={index}>
+                              <td className="border border-slate-300 p-4">
+                                {index + 1}
+                              </td>
+                              <td className="border border-slate-300 p-4">
+                                {i.description}
+                              </td>
+                              <td className="border border-slate-300 p-4">
+                                {i.currency}
+                              </td>
+                              <td className="border border-slate-300 p-4">
+                                {i.total}
+                              </td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      }
+                    </table>
+                    <div className="absolute right-20">
+                      <button
+                        className="text-2xl rounded-full text-green-600"
+                        onClick={(e) => {
+                          console.log("Here");
+                          setshowQuotation(true);
+                          ctx.setToggle();
+                        }}
+                        type="button"
+                      >
+                        <svg
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                          height={70}
+                          width={70}
+                        >
+                          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                          <g
+                            id="SVGRepo_tracerCarrier"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          ></g>
+                          <g id="SVGRepo_iconCarrier">
+                            <path
+                              opacity="0.5"
+                              d="M12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22Z"
+                              fill="#054d00"
+                            ></path>{" "}
+                            <path
+                              d="M12 8.25C12.4142 8.25 12.75 8.58579 12.75 9V11.25H15C15.4142 11.25 15.75 11.5858 15.75 12C15.75 12.4142 15.4142 12.75 15 12.75H12.75L12.75 15C12.75 15.4142 12.4142 15.75 12 15.75C11.5858 15.75 11.25 15.4142 11.25 15V12.75H9C8.58579 12.75 8.25 12.4142 8.25 12C8.25 11.5858 8.58579 11.25 9 11.25H11.25L11.25 9C11.25 8.58579 11.5858 8.25 12 8.25Z"
+                              fill="#054d00"
+                            ></path>{" "}
+                          </g>
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              )}
               <button
                 type="submit"
                 className="bg-blue-700 w-40 !mx-auto   text-white rounded-lg px-5 py-3 text-2xl self-center"
